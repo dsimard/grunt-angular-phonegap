2e10edd4ef31bed9ab40a7716a13d030df4d4c58
dsimard <dsimard@gmail.com>
2013-11-13 09:33:46 -0500

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+/*
+ * grunt-phonegapgap
+ * https://github.com/dsimard/grunt-phonegapgap
+ *
+ * Copyright (c) 2013 dsimard
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    coffee: {
+      options:{
+        sourceMap: true
+      },
+      compileTasks: {         
+        expand: true,
+        cwd: 'src',
+        src: ['{,**}/*.coffee'],
+        dest: 'tasks/',
+        ext: '.js'
+      }
+    },
+    watch: {
+      coffee: {
+        files: ['src/{,**}/*.coffee'],
+        tasks: ['coffee']
+      },
+      test: {
+        files: ['{test,tasks}/{,**}/*.js'],
+        tasks: ['test']
+      }
+    },
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    },
+
+    // Configuration to be run (and then tested).
+    phonegapgap: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+      custom_options: {
+        options: {
+          separator: ': ',
+          punctuation: ' !!!',
+        },
+        files: {
+          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js'],
+    },
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'phonegapgap', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ src/phonegapgap.coffee
@@ -0,0 +1,49 @@
+#
+# * grunt-phonegapgap
+# * https://github.com/dsimard/grunt-phonegapgap
+# *
+# * Copyright (c) 2013 dsimard
+# * Licensed under the MIT license.
+# 
+"use strict"
+module.exports = (grunt) ->
+  
+  # Please see the Grunt documentation for more information regarding task
+  # creation: http://gruntjs.com/creating-tasks
+  grunt.registerMultiTask "phonegapgap", "Grunt task for yeoman generator-phonegapgap", ->
+  
+    # Merge task-specific and/or target-specific options with these defaults.
+    options = @options(
+      punctuation: "."
+      separator: ", "
+    )
+    
+    # Iterate over all specified file groups.
+    @files.forEach (f) ->
+      
+      # Concat specified files.
+      
+      # Warn on and remove invalid source files (if nonull was set).
+      
+      # Read file source.
+      src = f.src.filter((filepath) ->
+        unless grunt.file.exists(filepath)
+          grunt.log.warn "Source file \"" + filepath + "\" not found."
+          false
+        else
+          true
+      ).map((filepath) ->
+        grunt.file.read filepath
+      ).join(grunt.util.normalizelf(options.separator))
+      
+      # Handle options.
+      src += options.punctuation
+      
+      # Write the destination file.
+      grunt.file.write f.dest, src
+      
+      # Print a success message.
+      grunt.log.writeln "File \"" + f.dest + "\" created."
+
+
+
